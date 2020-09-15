e607da0d018a2d2f71ea3834404013adfda47b48
zqrren <zqrren@outlook.com>
2020-09-15 21:57:20 +0800

    完成下载功能

--- src/main/java/com/ren/controller/FileController.java
+++ src/main/java/com/ren/controller/FileController.java
@@ -1,6 +1,7 @@
 package com.ren.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.ren.pojo.FilePojo;
 import com.ren.service.FileService;
 import com.ren.service.FileServiceImpl;
@@ -26,7 +27,10 @@ import java.io.*;
 import java.net.URLEncoder;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Date;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Controller
 public class FileController {
@@ -46,25 +50,55 @@ public class FileController {
 
     @RequestMapping("/download")
     public String download (String path, HttpServletResponse response) throws IOException {
-        System.out.println(path);
-        String name = Paths.get(path).getFileName().toString();
-        path = Paths.get(path).getParent().toString();
+        List<String> array = JSON.parseArray(path, String.class);
+        String name = System.currentTimeMillis()+".zip";
+        String zipPath = System.getProperty("nas") + name;
+        System.out.println(zipPath);
+        ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipPath));
+        for (String s : array) {
+            File file = Paths.get(s).toFile();
+            zip(zos,file,"");
+        }
+        zos.close();
         response.reset();
         response.setCharacterEncoding("UTF-8");
         response.setContentType("multipart/form-data");
         response.setHeader("Content-Disposition", "attachment;fileName="
                 + URLEncoder.encode(name, "UTF-8"));
-        File file = new File(path, name);
-        FileInputStream fis = new FileInputStream(file);
+        File file = new File(zipPath);
+        InputStream is = new FileInputStream(file);
+        BufferedInputStream bis = new BufferedInputStream(is);
         ServletOutputStream out = response.getOutputStream();
         byte[] buff = new byte[1024];
         int len = 0;
-        while ((len = fis.read(buff)) != -1) {
+        while ((len = bis.read(buff)) != -1) {
             out.write(buff, 0, len);
             out.flush();
         }
         out.close();
-        fis.close();
+        is.close();
+        bis.close();
+        boolean b = file.delete();
+        System.out.println(b);
         return "ok";
+    }
+    private void zip(ZipOutputStream out,File file,String path) throws IOException {
+        if (file.isDirectory()){
+            File[] files = file.listFiles();
+            assert files != null;
+            for (File f : files) {
+                zip(out,f,path+file.getName()+File.separator);
+            }
+        }
+        else {
+            out.putNextEntry(new ZipEntry(path+file.getName()));
+            FileInputStream fis = new FileInputStream(file);
+            int len = 0;
+            byte[] buffer = new byte[1024];
+            while ((len = fis.read(buffer))!=-1){
+                out.write(buffer,0,len);
+            }
+            fis.close();
+        }
     }
 }
